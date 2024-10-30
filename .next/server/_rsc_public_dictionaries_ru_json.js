b6dc98dead74b730a7a2b3112f9872e06ab19918
AssetGrav <417051@mail.ru>
2024-10-31 01:35:20 +0500

    remove headers

---
+++ .next/server/_rsc_public_dictionaries_ru_json.js
@@ -0,0 +1,25 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+exports.id = "_rsc_public_dictionaries_ru_json";
+exports.ids = ["_rsc_public_dictionaries_ru_json"];
+exports.modules = {
+
+/***/ "(rsc)/./public/dictionaries/ru.json":
+/*!*************************************!*\
+  !*** ./public/dictionaries/ru.json ***!
+  \*************************************/
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"navbar":{"navbarbrand":"О бренде","navbarproduct":"Продукция","navbaradventages":"Преймущества","navbaropinions":"Отзывы","navbarcontacts":"Контакты"},"head":{"headh1":"Alma-Le - Семейные ценности и профессиональная посуда на вашей кухне","headp":"Безопасная и высококачественная посуда, созданная для профессионалов и домашних кулинаров","headbutton":"Ознакомиться с ассортиментом"},"brand":{"head":"Ценности бренда","one":"Мы создали Alma Le вдохновившись теплыми семейными моментами на кухне","two":"Наша посуда не содержит вредных веществ и помогает готовить здоровую еду","three":"Изготовленна из меди, алюминия и нержавеющей стали для профессионалов и любителей"},"history":{"head":"Alma Le - рожденный из семейных традиций","one":" Alma Le - это бренд созданный в честь всех мам, которые посвещают бесценное время приготовлению пищи для своих близких... ","two":"Мы глубоко ценим семейные традиции и высокие моральные ценности, которые объединяют семьи.","three":"Покупая нашу посуду вы заботитесь о здоровье своих близких."},"mission":{"head":"Наша миссия профессиональная посуда для каждого дома","one":"мы стремимся предоставить посуду премиум-класса, чтобы каждый мог готовить с любовью и заботой."},"product":{"head":"Продукция","one":"Медный набор посуды ","two":"Alma Le 1.0","button":"Подробнее"},"advantages":{"head":"Преймущества","one":"Ручки не нагреваются, а равномерное распределение тепла помогает готовить быстрее","two":"Наша посуда не соджержит вредных веществ","three":"Медь, алюминий и нержавеющая сталь лудшие материалы для приготовления"},"opinion":{"head":"Что говорят профессионалы","one":"Основные преймущества продукции"},"form":{"head":"Форма обратной связи","answer":"Спасибо! Мы свяжемся с Вами в ближайшее время","name":"Имя","phone":"Номер телефона","email":"Электронная почта","message":"Сообщение","button":"Отправить","contact":"Контакты:","aemail":"Э-почта: almaleholding@gmail.com","aphone":"Мобильный: +7 700 978 80 08","aaddress":"Казахстан, Астана"},"footer":{"head":"Безопасная, высококачественная посуда, созданная для профессионалов и домашних кулинаров"},"basket":{"head":"Корзина","head2":"код отправлен на почту","delivery":"Доставка","deliveryLabel1":"Доставка по Казахстану","deliveryLabel2":"Доставка международная","total":"ИТОГО","name":"Ваше имя","email":"Электронная почта","phone":"Мобильный номер","address":"Адрес доставки","city":"Город","postIndex":"Почтовый индекс","button":"Оформить заказ","buttonSend":"Отправить","buttonClose":"Отмена","headVerif":"Ошибка верификации попробуйте заново","buttonVerifyClose":"Закрыть","headKaspi":" Оплата по KASPI","buttonKaspiBack":"назад","buttonToKaspi":"Перейти в приложение KASPI","errorName":"Имя Фамилия обязательны для заполнения","errorEmail":"Почта обязательны для заполнения","errorMobile":"Проверьте номер","errorMobileCount":"Только цифры"},"utensil":{"buttonBuy":"Купить","one":"Загрузка...","two":"Продукты не найдены"},"productAbout":{"buttonBack":"Назад","buttonBuy":"Купить набор"},"card":{"buttonMore":"Подробнее"}}');
+
+/***/ })
+
+};
+;
