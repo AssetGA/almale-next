29276eaf1412136d9f924185ce09f5b0dd794196
AssetGrav <417051@mail.ru>
2024-11-10 13:09:13 +0500

    add new meta

--- src/app/[lang]/page.js
+++ src/app/[lang]/page.js
@@ -1,10 +1,5 @@
 import Main from "../components/Main";
 
-export const metadata = {
-  name: "google-site-verification",
-  content: "rn1ZlN9g8g7fHia45p5G5UtdckF8ogxRywzzlJrHY-k",
-};
-
 export default function Home({ params: { lang } }) {
   return <Main lang={lang} />;
 }
