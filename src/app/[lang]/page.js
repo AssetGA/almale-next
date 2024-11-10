bcb7696cf44e779a66b1aa0ecc9bd86f081f64ca
AssetGrav <417051@mail.ru>
2024-11-10 12:53:47 +0500

    add to page

--- src/app/[lang]/page.js
+++ src/app/[lang]/page.js
@@ -1,5 +1,10 @@
 import Main from "../components/Main";
 
+export const metadata = {
+  name: "google-site-verification",
+  content: "rn1ZlN9g8g7fHia45p5G5UtdckF8ogxRywzzlJrHY-k",
+};
+
 export default function Home({ params: { lang } }) {
   return <Main lang={lang} />;
 }
