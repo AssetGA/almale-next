a1582486a5a0b99c1478c1a9df477de840337caa
AssetGrav <417051@mail.ru>
2024-11-20 04:08:22 +0500

    add correct meta

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,26 @@
-/** @type {import('next-sitemap').IConfig} */
+const axios = require("axios");
 
 module.exports = {
   siteUrl: process.env.NEXT_PUBLIC_SITE_URL || "https://alma-le.com",
-  generateRobotsTxt: true,
-  sitemapSize: 7000,
+  generateRobotsTxt: true, // Создать robots.txt
+  changefreq: "daily",
+  priority: 0.7,
+  exclude: ["/api/*", "/_next/*", "/img/*", "/fonts/*", "/favicon.ico"],
+  transform: async (config, path) => {
+    // Локали для маршрутов
+    const locales = ["kz", "en", "ru"];
+    const defaultLocale = "kz";
+
+    // Пример: обработка статических маршрутов
+    if (path === "/") {
+      return locales.map((locale) => ({
+        loc: `/${locale}${path}`,
+        lastmod: new Date().toISOString(),
+        changefreq: config.changefreq,
+        priority: config.priority,
+      }));
+    }
+
+    return null; // Пусть другие маршруты обрабатываются как есть
+  },
 };
