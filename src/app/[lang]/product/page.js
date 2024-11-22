66062d6f29bdfbac805bdc83cf37f08a0ec6648b
AssetGrav <417051@mail.ru>
2024-11-23 02:58:57 +0500

    add meta

--- src/app/[lang]/product/page.js
+++ src/app/[lang]/product/page.js
@@ -1,14 +1,29 @@
 import UtensilsSet from "../../components/UtensilsSet";
 import { getDictionary } from "../dictionaries";
 
-export const metadata = {
-  title: "Alma Le - Набор профессиональной посуды премиум класса",
-  description:
-    "Откройте для себя набор посуды Alma Le. Кастрюли и сковородки с внутренним покрытием из нержавеющей стали и внешним из алюминия и меди.",
-  keywords:
-    "AlmaLe, посуда, набор посуды, кастрюли, сковородки, сотейники, нержавеющая сталь, алюминий, медь, кухня",
-  // robots: "index, follow",
-};
+export async function getServerSideProps({ req }) {
+  // Извлекаем язык из URL
+  const { pathname } = req;
+  const langMatch = pathname.match(/^\/(kz|en|ru)(\/|$)/);
+  const lang = langMatch ? langMatch[1] : "kz"; // Если язык найден, берем его, иначе — 'kz'
+
+  // Возвращаем данные как props для компонента
+  return {
+    props: {
+      lang,
+    },
+  };
+}
+
+export async function generateMetadata({ params }) {
+  const t = await getDictionary(params.lang);
+
+  return {
+    title: `${t.metadataProduct.title} - Alma Le`,
+    description: t.metadataProduct.description,
+    keywords: t.metadataProduct.keywords,
+  };
+}
 
 export default async function Page({ params: { lang } }) {
   const t = await getDictionary(lang);
