f1b5e7e31b5421580dae91a5445b135d8f801a58
AssetGrav <417051@mail.ru>
2025-02-13 16:33:57 +0500

    button to moroz add

--- src/app/lib/api/contact/route.js
+++ src/app/lib/api/contact/route.js
@@ -40,3 +40,40 @@ export async function GET(request) {
     );
   }
 }
+
+export async function POST(request) {
+  const origin = request.headers.get("origin");
+
+  if (origin !== process.env.NEXT_PUBLIC_SITE_URL) {
+    return new NextResponse("Access Denied", { status: 403 });
+  }
+  const { name, contact, message, type } = await request.json();
+  try {
+    const headers = {
+      "Access-Control-Allow-Origin": `${process.env.NEXT_PUBLIC_SITE_URL}`,
+      "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE",
+      "Access-Control-Allow-Headers":
+        "Origin, X-Requested-With, Content-Type, Accept",
+    };
+    // Обрабатываем запрос OPTIONS для CORS preflight
+    if (request.method === "OPTIONS") {
+      return NextResponse.json(null, { status: 200, headers });
+    }
+
+    // Логика обработки
+    const getInfo = await sendMessage(
+      `Имя ${
+        type === "moroz" ? "Дед мороза" : "Волшебника"
+      }: ${name}, контакт по: ${contact}  сообщение: ${message}`
+    );
+
+    return NextResponse.json({ message: getInfo }, { status: 200, headers });
+  } catch (e) {
+    return NextResponse.json(
+      {
+        message: "На сервере произошла ошибка попробуйте позже",
+      },
+      { status: 500 }
+    );
+  }
+}
