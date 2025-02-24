9de060fc0233eb7d1ac61e4f99ab091845599312
AssetGrav <417051@mail.ru>
2025-02-25 00:46:14 +0500

    add  info

--- src/middleware.js
+++ src/middleware.js
@@ -7,7 +7,7 @@ const protectedRoutes = ["/basket", "/payment"];
 const publicRoutes = ["/product", "/login", "/signup", "/"];
 
 let locales = ["kz", "en", "ru"];
-let defaultLocale = "en";
+let defaultLocale = "kz";
 
 // Get the preferred locale, similar to the above or using a library
 function getLocale() {
