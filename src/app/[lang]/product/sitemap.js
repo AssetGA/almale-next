1f342dc0497ef8af3e0c88c81d9a39601b658f9a
AssetGrav <417051@mail.ru>
2025-03-03 12:26:42 +0500

    add new sitemap

--- src/app/[lang]/product/sitemap.js
+++ src/app/[lang]/product/sitemap.js
@@ -1,20 +1,23 @@
 const SUPPORTED_LANGS = ["kz", "en", "ru"];
 
 export async function generateSitemaps() {
-  const params = [];
-
-  await Promise.all(
-    SUPPORTED_LANGS.map(async (lang) => {
-      params.push({ lang });
-    })
-  );
-
-  return params; // Return parameters for static generation
+  // This can be simplified as we're not doing any asynchronous operations
+  const params = SUPPORTED_LANGS.map((lang) => ({ lang }));
+  return params;
 }
 
 export default async function sitemap({ lang }) {
-  return {
-    url: `${process.env.NEXT_PUBLIC_SITE_URL}/${lang}/product`,
-    lastModified: new Date().toISOString(),
-  };
+  // Ensure NEXT_PUBLIC_SITE_URL is defined
+  if (!process.env.NEXT_PUBLIC_SITE_URL) {
+    console.error("NEXT_PUBLIC_SITE_URL is not defined");
+    return [];
+  }
+
+  // Generate sitemap entry for the specific language
+  return [
+    {
+      url: `${process.env.NEXT_PUBLIC_SITE_URL}/${lang}/product`,
+      lastModified: new Date().toISOString(),
+    },
+  ];
 }
