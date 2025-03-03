ea19fe01449f0ece8843bacd5c7afcf9309c9417
AssetGrav <417051@mail.ru>
2025-03-03 11:28:59 +0500

    add new sitemap

---
+++ src/app/sitemap-index.js
@@ -0,0 +1,12 @@
+export default function sitemapIndex() {
+  return [
+    {
+      url: "https://www.alma-le.com/sitemap.xml",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.alma-le.com/videos/sitemap.xml",
+      lastModified: new Date(),
+    },
+  ];
+}
